b5c7e28e27a4acf27a09bd3507e731cd199a797d
ufo6688 <maliaogpt@outlook.com>
2025-08-15 15:27:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,10 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    },
     ckzy: {
          api: 'https://www.ckzy1.com/api.php/provide/vod',
          name: 'CK资源',
