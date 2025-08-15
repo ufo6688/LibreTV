73f801062dbcb0cc81e2a7868e990aa768b25e67
ufo6688 <maliaogpt@outlook.com>
2025-08-15 15:14:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,8 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-//以下为成人源
-    testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
         adult: true
