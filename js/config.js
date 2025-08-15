9498101f2f40680036520d33f940e51ac13098a3
ufo6688 <maliaogpt@outlook.com>
2025-08-15 15:05:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-     dyttzy: {
+    dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
@@ -114,6 +114,54 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
+//以下为成人源
+    testSource: {
+        api: 'https://www.example.com/api.php/provide/vod',
+        name: '空内容测试源',
+        adult: true
+    },
+    ckzy: {
+         api: 'https://www.ckzy1.com/api.php/provide/vod',
+         name: 'CK资源',
+         adult: true
+     },
+     jkun: {
+         api: 'https://jkunzyapi.com/api.php/provide/vod',
+         name: 'jkun资源',
+         adult: true
+     },
+     bwzy: {
+         api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+         name: '百万资源',
+         adult: true
+     },
+     souav: {
+         api: 'https://api.souavzy.vip/api.php/provide/vod',
+         name: 'souav资源',
+         adult: true
+     },
+     r155: {
+         api: 'https://155api.com/api.php/provide/vod',
+         name: '155资源',
+         adult: true
+     },
+     lsb: {
+         api: 'https://apilsbzy1.com/api.php/provide/vod',
+         name: 'lsb资源',
+        adult: true
+    },
+     huangcang: {
+         api: 'https://hsckzy.vip/api.php/provide/vod',
+        name: '黄色仓库',
+         adult: true,
+         detail: 'https://hsckzy.vip'
+     },
+     yutu: {
+         api: 'https://yutuzy10.com/api.php/provide/vod',
+         name: '玉兔资源',
+         adult: true
+     },
+};
 testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
